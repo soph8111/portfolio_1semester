b65c7f205e78c2b7892a7d0a37e8eedb921e0ef4
soph8111 <soph8111@stud.kea.dk>
2021-01-06 10:07:34 +0100

    kodning so far til github

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    // Tryk på burgermenu
+    document.querySelector("#burgermenu").addEventListener("click", toggelmenu);
+}
+
+function toggelmenu() {
+    console.log("toggelmenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+    // Animation ved åben af menu
+    document.querySelector("#menu").classList.toggle("dropDown_open");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#burgermenu").textContent = "☰";
+    } else {
+        document.querySelector("#burgermenu").textContent = "╳";
+    }
+}
